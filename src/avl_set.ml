69acac1ede5cae18cb07d91ea225f661d15115bc
sidimekov <dimasidim8582@gmail.com>
2025-11-25 17:17:51 +0300

    done avl and tests

--- src/avl_set.ml
+++ src/avl_set.ml
@@ -36,71 +36,202 @@ end
 module Make (Ord : OrderedType) : S with type elt = Ord.t = struct
   type elt = Ord.t
 
-  (* Временная простейшая реализация через список.
-     На этапе 3 заменим на настоящее AVL-дерево и перепишем все функции. *)
+  type t =
+    | Empty
+    | Node of {
+        left : t;
+        value : elt;
+        right : t;
+        height : int;
+      }
 
-  type t = elt list
+  let empty = Empty
 
-  let empty = []
+  let is_empty = function
+    | Empty -> true
+    | _ -> false
 
-  let is_empty s = (s = [])
+  let height = function
+    | Empty -> 0
+    | Node { height; _ } -> height
 
-  let mem x s = List.exists (fun y -> Ord.compare x y = 0) s
+  let make left value right =
+    let hl = height left in
+    let hr = height right in
+    let h = 1 + max hl hr in
+    Node { left; value; right; height = h }
 
-  let add x s =
-    if mem x s then s else x :: s
+  let balance_factor = function
+    | Empty -> 0
+    | Node { left; right; _ } -> height left - height right
 
-  let remove x s =
-    List.filter (fun y -> Ord.compare x y <> 0) s
+  let rotate_right = function
+    | Node { left = Node { left = a; value = x; right = b; _ }; value = y; right = c; _ } ->
+        make a x (make b y c)
+    | _ -> failwith "rotate_right: invalid structure"
 
-  let union a b =
-    List.fold_left (fun acc x -> add x acc) a b
+  let rotate_left = function
+    | Node { left = a; value = x; right = Node { left = b; value = y; right = c; _ }; _ } ->
+        make (make a x b) y c
+    | _ -> failwith "rotate_left: invalid structure"
 
-  let inter a b =
-    List.filter (fun x -> mem x b) a
+  let balance t =
+    match t with
+    | Empty -> Empty
+    | Node { left; value; right; _ } as node ->
+        let bf = balance_factor node in
+        if bf > 1 then
+          (* левое поддерево слишком высокое *)
+          match left with
+          | Empty -> node
+          | _ ->
+              if balance_factor left < 0 then
+                (* большой правый поворот *)
+                let left' = rotate_left left in
+                rotate_right (make left' value right)
+              else
+                rotate_right node
+        else if bf < -1 then
+          (* правое поддерево слишком высокое *)
+          match right with
+          | Empty -> node
+          | _ ->
+              if balance_factor right > 0 then
+                (* большой левый поворот *)
+                let right' = rotate_right right in
+                rotate_left (make left value right')
+              else
+                rotate_left node
+        else
+          (* уже сбалансировано, просто пересчитываем высоту *)
+          make left value right
 
-  let diff a b =
-    List.filter (fun x -> not (mem x b)) a
+  let rec mem x = function
+    | Empty -> false
+    | Node { left; value; right; _ } ->
+        let c = Ord.compare x value in
+        if c = 0 then true
+        else if c < 0 then mem x left
+        else mem x right
 
-  let cardinal s = List.length s
+  let rec add x = function
+    | Empty -> make Empty x Empty
+    | Node { left; value; right; _ } as node ->
+        let c = Ord.compare x value in
+        if c = 0 then
+          node
+        else if c < 0 then
+          balance (make (add x left) value right)
+        else
+          balance (make left value (add x right))
 
-  let iter f s = List.iter f s
+  (* remove_min: вернуть минимальное значение и дерево без него *)
+  let rec remove_min = function
+    | Empty -> failwith "remove_min on empty"
+    | Node { left = Empty; value; right; _ } ->
+        (value, right)
+    | Node { left; value; right; _ } ->
+        let m, left' = remove_min left in
+        (m, balance (make left' value right))
 
-  let fold_left f acc s = List.fold_left f acc s
+  let rec remove x = function
+    | Empty -> Empty
+    | Node { left; value; right; _ } ->
+        let c = Ord.compare x value in
+        if c < 0 then
+          balance (make (remove x left) value right)
+        else if c > 0 then
+          balance (make left value (remove x right))
+        else
+          (* удаляем этот узел *)
+          match left, right with
+          | Empty, _ -> right
+          | _, Empty -> left
+          | _ ->
+              (* берём минимальный элемент из правого поддерева *)
+              let m, right' = remove_min right in
+              balance (make left m right')
 
-  let fold_right f s acc = List.fold_right f s acc
+  let rec iter f = function
+    | Empty -> ()
+    | Node { left; value; right; _ } ->
+        iter f left;
+        f value;
+        iter f right
 
-  let filter p s = List.filter p s
+  let fold_left f init t =
+    let acc = ref init in
+    iter (fun x -> acc := f !acc x) t;
+    !acc
 
-  let map f s =
-    (* NB: на этапе 3 надо будет учесть порядок и дубликаты. *)
-    let mapped = List.map f s in
-    List.fold_left (fun acc x -> add x acc) empty mapped
+  let fold_right f t init =
+    let rec aux acc = function
+      | Empty -> acc
+      | Node { left; value; right; _ } ->
+          let acc1 = aux acc right in
+          aux (f value acc1) left
+    in
+    aux init t
 
-  let to_list s =
-    (* Для детерминированности отсортируем по Ord.compare *)
-    List.sort Ord.compare s
+  let cardinal t =
+    fold_left (fun acc _ -> acc + 1) 0 t
+
+  let filter p t =
+    fold_left
+      (fun acc x -> if p x then add x acc else acc)
+      empty t
+
+  let map f t =
+    fold_left
+      (fun acc x -> add (f x) acc)
+      empty t
+
+  let to_list t =
+    fold_left (fun acc x -> x :: acc) [] t
+    |> List.rev
 
   let of_list l =
     List.fold_left (fun acc x -> add x acc) empty l
 
+  let union a b =
+    if cardinal a <= cardinal b then
+      fold_left (fun acc x -> add x acc) b a
+    else
+      fold_left (fun acc x -> add x acc) a b
+
+  let inter a b =
+    fold_left
+      (fun acc x -> if mem x b then add x acc else acc)
+      empty a
+
+  let diff a b =
+    fold_left
+      (fun acc x -> if mem x b then acc else add x acc)
+      empty a
+
   let equal a b =
-    let a' = to_list a in
-    let b' = to_list b in
-    List.length a' = List.length b'
-    && List.for_all2 (fun x y -> Ord.compare x y = 0) a' b'
+    let la = to_list a in
+    let lb = to_list b in
+    let rec loop xs ys =
+      match xs, ys with
+      | [], [] -> true
+      | x :: xs', y :: ys' ->
+          Ord.compare x y = 0 && loop xs' ys'
+      | _ -> false
+    in
+    loop la lb
 
   let compare a b =
-    let a' = to_list a in
-    let b' = to_list b in
+    let la = to_list a in
+    let lb = to_list b in
     let rec loop xs ys =
       match xs, ys with
       | [], [] -> 0
       | [], _ -> -1
       | _, [] -> 1
       | x :: xs', y :: ys' ->
-        let c = Ord.compare x y in
-        if c <> 0 then c else loop xs' ys'
+          let c = Ord.compare x y in
+          if c <> 0 then c else loop xs' ys'
     in
-    loop a' b'
+    loop la lb
 end
--- test/avl_set_test.ml
+++ test/avl_set_test.ml
@@ -1,3 +1,122 @@
+module IntOrd = struct
+  type t = int
+  let compare = Int.compare
+end
+
+module IntSet = Avl_set.Make (IntOrd)
+
+open Alcotest
+
+(* ---------- Unit-тесты ---------- *)
+
+let test_empty () =
+  check bool "empty is empty" true (IntSet.is_empty IntSet.empty)
+
+let test_add_mem () =
+  let s = IntSet.empty |> IntSet.add 1 |> IntSet.add 2 in
+  check bool "mem 1" true (IntSet.mem 1 s);
+  check bool "mem 2" true (IntSet.mem 2 s);
+  check bool "mem 3" false (IntSet.mem 3 s)
+
+let test_add_no_duplicates () =
+  let s = IntSet.empty |> IntSet.add 1 |> IntSet.add 1 in
+  check int "cardinal after dup adds" 1 (IntSet.cardinal s)
+
+let test_remove () =
+  let s = IntSet.(empty |> add 1 |> add 2 |> add 3) in
+  let s' = IntSet.remove 2 s in
+  check bool "2 removed" false (IntSet.mem 2 s');
+  check bool "1 still there" true (IntSet.mem 1 s');
+  check bool "3 still there" true (IntSet.mem 3 s')
+
+let test_of_list_to_list_sorted () =
+  let s = IntSet.of_list [ 3; 1; 2; 2 ] in
+  let lst = IntSet.to_list s in
+  check (list int) "sorted unique" [ 1; 2; 3 ] lst
+
+let unit_tests =
+  [
+    test_case "empty" `Quick test_empty;
+    test_case "add/mem" `Quick test_add_mem;
+    test_case "add no duplicates" `Quick test_add_no_duplicates;
+    test_case "remove" `Quick test_remove;
+    test_case "of_list/to_list" `Quick test_of_list_to_list_sorted;
+  ]
+
+(* ---------- Property-based тесты (QCheck) ---------- *)
+
+open QCheck
+
+let gen_int_set =
+  let open QCheck in
+  map IntSet.of_list (list small_int)
+
+(* Свойства моноида: empty — нейтральный элемент для union *)
+
+let prop_monoid_left_identity =
+  Test.make
+    ~name:"union empty s = s"
+    gen_int_set
+    (fun s -> IntSet.equal (IntSet.union IntSet.empty s) s)
+
+let prop_monoid_right_identity =
+  Test.make
+    ~name:"union s empty = s"
+    gen_int_set
+    (fun s -> IntSet.equal (IntSet.union s IntSet.empty) s)
+
+let gen3_int_set =
+  QCheck.triple gen_int_set gen_int_set gen_int_set
+
+let prop_monoid_associativity =
+  Test.make
+    ~name:"union associative"
+    gen3_int_set
+    (fun (a, b, c) ->
+      let left = IntSet.union (IntSet.union a b) c in
+      let right = IntSet.union a (IntSet.union b c) in
+      IntSet.equal left right)
+
+(* Дополнительные свойства множества *)
+
+let prop_cardinal_le_list_length =
+  Test.make
+    ~name:"cardinal (of_list l) <= length l"
+    (QCheck.list QCheck.small_int)
+    (fun l ->
+      let s = IntSet.of_list l in
+      IntSet.cardinal s <= List.length l)
+
+let prop_union_idempotent =
+  Test.make
+    ~name:"union s s = s"
+    gen_int_set
+    (fun s -> IntSet.equal (IntSet.union s s) s)
+
+let prop_mem_of_list =
+  Test.make
+    ~name:"mem x (of_list l) <-> List.mem x l"
+    (pair small_int (list small_int))
+    (fun (x, l) ->
+      let s = IntSet.of_list l in
+      IntSet.mem x s = List.mem x l)
+
+let qcheck_tests =
+  QCheck_alcotest.to_alcotest
+    [
+      prop_monoid_left_identity;
+      prop_monoid_right_identity;
+      prop_monoid_associativity;
+      prop_cardinal_le_list_length;
+      prop_union_idempotent;
+      prop_mem_of_list;
+    ]
+
+(* ---------- Запуск тестов ---------- *)
+
 let () =
-  (* Заглушка: если программа завершилась без исключений, тест считается пройденным *)
-  ()
+  Alcotest.run "avl_set"
+    [
+      ("unit", unit_tests);
+      ("properties", qcheck_tests);
+    ]
