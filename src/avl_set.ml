4626af4d5a1dcfabbad3d9579b45199e38596a05
sidimekov <dimasidim8582@gmail.com>
2025-11-25 17:01:59 +0300

    init project

---
+++ src/avl_set.ml
@@ -0,0 +1,14 @@
+module type OrderedType = sig
+  type t
+  val compare : t -> t -> int
+end
+
+module type S = sig
+  type elt
+  type t
+end
+
+module Make (Ord : OrderedType) : S with type elt = Ord.t = struct
+  type elt = Ord.t
+  type t = unit
+end
